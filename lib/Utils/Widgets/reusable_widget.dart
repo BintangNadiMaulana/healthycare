205bb3ed67a4423b87b5d533bf479db92adeec44
AhmadWahyudi <ahmad.wahyudi@allianz.co.id>
2023-05-07 10:48:37 +0700

    custom button add

--- lib/Utils/Widgets/reusable_widget.dart
+++ lib/Utils/Widgets/reusable_widget.dart
@@ -30,4 +30,22 @@ class ResUseAbleWidget {
     );
   }
 
+  Widget customButton(String text) {
+    return Container(
+      height: 48,
+      margin: EdgeInsets.only(right: 20),
+      child: ElevatedButton(
+          onPressed: (){},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(child: Container()),
+              Expanded(child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),)),
+              Icon(Icons.arrow_forward, color: Colors.white,)
+
+            ],
+          )),
+    );
+  }
+
 }
